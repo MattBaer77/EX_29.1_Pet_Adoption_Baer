4f3aa9bb7f000c1eb75a210d5af74b42996ca39b
Matthew Baer <matt.baer77@gmail.com>
2023-03-06 23:14:43 -0500

    added templates and began home route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 # from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
 # from forms import ###
@@ -13,3 +13,13 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 # debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+@app.route('/')
+def home_list_pets():
+    """Render home page - a list of pets"""
+
+    # Get pets from db
+    all_pets = Pets.query.all()
+
+    # Pass into template
+    render_template("home.html", pets=all_pets)
