24fe7561cbbe5f541a33bbcb24cb7d6f69c4649e
Matthew Baer <matt.baer77@gmail.com>
2023-03-06 22:49:20 -0500

    modified pet images seed.py

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-from models import Pet
+from models import Pet, db
 from app import app
 
 # Delete all tables
@@ -10,11 +10,11 @@ Pet.query.delete()
 
 # Add sample pets
 
-Ladybird = Pet(name= 'LadyBird', species='Dog', photo_url='/Ladybird.JPG' age='6', available=False)
-Tank = Pet(name= 'Tank', species='Dog', photo_url='/Pepper.JPG' age='7', available=True)
-Pepper = Pet(name= 'Pepper', species='Dog', photo_url='/Percy.JPG' age='6')
-Percy = Pet(name= 'Percy', species='Dog', photo_url='Tank.jpeg' age='3')
-Wesson = Pet(name= 'Wesson', species='Dog', photo_url='Wesson.jpeg' age='7')
+Ladybird = Pet(name= 'LadyBird', species='Dog', photo_url='/Ladybird.JPG', age='6', available=False)
+Tank = Pet(name= 'Tank', species='Dog', photo_url='/Tank.jpeg', age='7', available=True)
+Pepper = Pet(name= 'Pepper', species='Dog', photo_url='/Pepper.JPG', age='6')
+Percy = Pet(name= 'Percy', species='Dog', photo_url='/Percy.JPG', age='3')
+Wesson = Pet(name= 'Wesson', species='Dog', photo_url='/Wesson.jpeg', age='7')
 
-db.session.add_all([LadyBird, Tank, Pepper, Percy, Wesson])
+db.session.add_all([Ladybird, Tank, Pepper, Percy, Wesson])
 db.session.commit()
